1dbffe07e8ddcd777883fb7471dab4a67b0d20cf
Cristiano Pacheco <chris.spb25@gmail.com>
2022-10-17 21:48:10 -0300

    Added the bootstrap template

--- controller/home/index.go
+++ controller/home/index.go
@@ -1,11 +1,23 @@
 package controller
 
 import (
-	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
 func HomeIndexAction(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Oh Yeah!</h1>")
+	files := []string{"./view/layout/bootstrap.gohtml", "./view/home/index.gohtml"}
+	templateSet, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal server error", 500)
+		return
+	}
+	err = templateSet.ExecuteTemplate(w, "bootstrap", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal server error", 500)
+	}
 }
