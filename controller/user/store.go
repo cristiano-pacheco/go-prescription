b06d4586dc425f5a804638a7bf57161ca6f4e22d
Cristiano Pacheco <chris.spb25@gmail.com>
2022-10-24 17:08:42 -0300

    Added the user validator

--- controller/user/store.go
+++ controller/user/store.go
@@ -5,10 +5,17 @@ import (
 	"net/http"
 
 	"github.com/cristiano-pacheco/go-prescription/model"
+	"github.com/cristiano-pacheco/go-prescription/validator"
+	"github.com/cristiano-pacheco/go-prescription/view"
 )
 
 type userStoreAction struct {
 	userModel *model.UserModel
+}
+
+type userCreateForm struct {
+	Name   string
+	Errors map[string]string
 }
 
 func NewUserStoreAction(userModel *model.UserModel) *userStoreAction {
@@ -21,6 +28,22 @@ func (action *userStoreAction) ServeHTTP(w http.ResponseWriter, r *http.Request)
 		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
+
+	uv := validator.UserValidator{}
+	validationErrors := uv.Validate(r.PostForm.Get("name"))
+	form := userCreateForm{
+		Name:   r.PostForm.Get("name"),
+		Errors: validationErrors,
+	}
+
+	templateData := view.TemplateData{}
+	templateData.Form = form
+
+	if len(validationErrors) > 0 {
+		view.NewTemplate().RenderFormWithValidationErrors(w, &templateData)
+		return
+	}
+
 	_, err = action.userModel.Insert(r.PostForm.Get("name"))
 	if err != nil {
 		log.Printf("An error ocurriend during the user creation insert: %s", err)
--- view/view.go
+++ view/view.go
@@ -12,6 +12,10 @@ func NewTemplate() *Template {
 
 type Template struct{}
 
+type TemplateData struct {
+	Form any
+}
+
 func (t *Template) Render(w http.ResponseWriter, data interface{}, templates ...string) {
 	templateSet, err := template.ParseFiles(templates...)
 	if err != nil {
@@ -24,4 +28,10 @@ func (t *Template) Render(w http.ResponseWriter, data interface{}, templates ...
 		log.Print(err.Error())
 		http.Error(w, "Internal server error", 500)
 	}
+}
+
+func (t *Template) RenderFormWithValidationErrors(w http.ResponseWriter, templateData *TemplateData) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusUnprocessableEntity)
+	t.Render(w, templateData, "./view/layout/bootstrap.gohtml", "./view/user/create.gohtml")
 }
