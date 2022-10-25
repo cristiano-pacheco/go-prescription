8d318046b69e1c865cdf6041b8ea00e56fbf6fc7
Cristiano Pacheco <chris.spb25@gmail.com>
2022-10-24 22:05:46 -0300

    user edit page

--- controller/user/edit.go
+++ controller/user/edit.go
@@ -2,7 +2,9 @@ package controller
 
 import (
 	"net/http"
+	"strconv"
 
+	"github.com/alexedwards/flow"
 	"github.com/cristiano-pacheco/go-prescription/model"
 	"github.com/cristiano-pacheco/go-prescription/view"
 )
@@ -15,7 +17,22 @@ func NewUserEditAction(userModel *model.UserModel) *userEditAction {
 	return &userEditAction{userModel: userModel}
 }
 
+type userUpdateForm struct {
+	Name   string
+	Errors map[string]string
+}
+
 func (action *userEditAction) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	view.NewTemplate().Render(w, nil, "./view/layout/bootstrap.gohtml", "./view/user/edit.gohtml")
+	id, err := strconv.Atoi(flow.Param(r.Context(), "id"))
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+	user, err := action.userModel.Get(uint(id))
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	view.NewTemplate().Render(w, user, "./view/layout/bootstrap.gohtml", "./view/user/edit.gohtml")
 }
