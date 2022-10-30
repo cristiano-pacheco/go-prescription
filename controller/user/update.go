eb279bcf8a771fcd68d6b9b378211597a34acdd8
Cristiano Pacheco <chris.spb25@gmail.com>
2022-10-30 12:12:33 -0300

    Add docs to the user udpate action

--- controller/user/update.go
+++ controller/user/update.go
@@ -19,16 +19,20 @@ func NewUserUpdateAction(userModel *model.UserModel) *userUpdateAction {
 }
 
 func (action *userUpdateAction) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// Parse the user ID from URL
 	id, err := strconv.Atoi(flow.Param(r.Context(), "id"))
 	if err != nil {
 		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 	}
 
+	// Parse the form
 	err = r.ParseForm()
 	if err != nil {
 		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
+
+	// Validates the form data
 	uv := validator.UserValidator{}
 	validationErrors := uv.Validate(r.PostForm.Get("name"))
 	form := userUpdateForm{
@@ -37,13 +41,18 @@ func (action *userUpdateAction) ServeHTTP(w http.ResponseWriter, r *http.Request
 	}
 	templateData := view.TemplateData{}
 	templateData.Form = form
+
+	// If the form data is invalid, render the page with the validation errors
 	if len(validationErrors) > 0 {
 		view.NewTemplate().RenderFormWithValidationErrors(w, &templateData)
 		return
 	}
+
+	// Updates the user in the database
 	err = action.userModel.Update(uint(id), r.PostForm.Get("name"))
 	if err != nil {
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 	}
+
 	http.Redirect(w, r, "/users", http.StatusSeeOther)
 }
