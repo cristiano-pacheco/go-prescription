8548534118a7feb1127afc6d908c3138e4102f79
Cristiano Pacheco <chris.spb25@gmail.com>
2022-10-30 12:08:49 -0300

    user update

--- controller/user/update.go
+++ controller/user/update.go
@@ -2,8 +2,12 @@ package controller
 
 import (
 	"net/http"
+	"strconv"
 
+	"github.com/alexedwards/flow"
 	"github.com/cristiano-pacheco/go-prescription/model"
+	"github.com/cristiano-pacheco/go-prescription/validator"
+	"github.com/cristiano-pacheco/go-prescription/view"
 )
 
 type userUpdateAction struct {
@@ -15,10 +19,31 @@ func NewUserUpdateAction(userModel *model.UserModel) *userUpdateAction {
 }
 
 func (action *userUpdateAction) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
+	id, err := strconv.Atoi(flow.Param(r.Context(), "id"))
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+	}
+
+	err = r.ParseForm()
 	if err != nil {
 		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
-
+	uv := validator.UserValidator{}
+	validationErrors := uv.Validate(r.PostForm.Get("name"))
+	form := userUpdateForm{
+		Name:   r.PostForm.Get("name"),
+		Errors: validationErrors,
+	}
+	templateData := view.TemplateData{}
+	templateData.Form = form
+	if len(validationErrors) > 0 {
+		view.NewTemplate().RenderFormWithValidationErrors(w, &templateData)
+		return
+	}
+	err = action.userModel.Update(uint(id), r.PostForm.Get("name"))
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	}
+	http.Redirect(w, r, "/users", http.StatusSeeOther)
 }
